d503d4e1eaa458b98532558de45dc2f8cf3bd411
Hussein Talal <hossientalal2@gmail.com>
2020-06-22 08:02:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-class Formatter {
+
   //add static methods here
-}
+  class Formatter {
+    static capitalize(word){
+      return word[0].toUpperCase() + word.split("").slice(1).join("")
+    }
+    static sanitize(word){
+      return word.replace(/\W/g,"")
+    }
+    static titleize(str){
+      return str.map(word => {
+          if(word === "the" ||word === "a" ||word === "an" ||word === "but" ||word === "of" ||word === "and" ||word === "for" ||word === "at" ||word === "by" ||word === "from" ) return word
+          return word[0].toUpperCase() + word.split("").slice(1).join("")
+      })
+    }
+  }
